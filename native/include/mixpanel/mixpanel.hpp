b588f64973e54a95f7f485fda7376e3bd78fec63
samgreen <greensamuelm@gmail.com>
2016-06-16 15:29:54 -0700

    Bump version number.

--- native/include/mixpanel/mixpanel.hpp
+++ native/include/mixpanel/mixpanel.hpp
@@ -1,6 +1,6 @@
 /*
     *** do not modify the line below, it is updated by the build scripts ***
-    Mixpanel C++ SDK version v1.0.0
+    Mixpanel C++ SDK version v1.0.1
 */
 
 #ifndef _MIXPANEL_HPP_
