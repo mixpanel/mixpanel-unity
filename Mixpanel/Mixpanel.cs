f315eb3a9bf85e55dd934c853f2d8bc20b58a366
Kyle Rockman <kyle.rockman@mac.com>
2019-07-16 09:51:52 -0500

    Switch to seperate auto properties for track and engage api calls

--- Mixpanel/Mixpanel.cs
+++ Mixpanel/Mixpanel.cs
@@ -7,22 +7,28 @@ using UnityEngine.Networking;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
+#if UNITY_IOS
+using UnityEngine.iOS;
+#endif
 
 namespace mixpanel
 {
     public static partial class Mixpanel
     {
+        private const string MixpanelUnityVersion = "2.0.0";
+        
         private const string MixpanelTrackUrl = "https://api.mixpanel.com/track";
         private const string MixpanelEngageUrl = "https://api.mixpanel.com/engage";
         
         internal static Value OnceProperties = Value.Object;
-        internal static Value AutoProperties;
+        internal static Value AutoTrackProperties;
+        internal static Value AutoEngageProperties;
 
         internal static void DoTrack(string eventName, Value properties)
         {
             if (!IsTracking) return;
-            if (AutoProperties == null) AutoProperties = CollectAutoProperties();
-            properties.Merge(AutoProperties);
+            if (AutoTrackProperties == null) AutoTrackProperties = CollectAutoTrackProperties();
+            properties.Merge(AutoTrackProperties);
             // These auto properties can change in runtime so we don't bake them into AutoProperties
             properties["$screen_width"] = Screen.width;
             properties["$screen_height"] = Screen.height;
@@ -41,29 +47,71 @@ namespace mixpanel
             Enqueue(MixpanelTrackUrl, data);
         }
 
-        internal static Value CollectAutoProperties()
+        internal static Value CollectAutoTrackProperties()
         {
             Value properties = new Value
             {
-                {"$app_build_number", Application.version},
-                {"$app_version", Application.unityVersion},
-                {"$device", Application.platform.ToString()},
-                {"$model", SystemInfo.deviceModel},
+                {"mp_lib", "unity"},
+                {"$lib_version", MixpanelUnityVersion},
                 {"$os", SystemInfo.operatingSystemFamily.ToString()},
                 {"$os_version", SystemInfo.operatingSystem},
-                {"$screen_dpi", Screen.dpi},
+                {"$manufacturer", ""},
+                {"$model", SystemInfo.deviceModel},
+                {"$app_version_string", Application.unityVersion},
+                {"$app_build_number", Application.version},
+                {"$carrier", ""},
                 {"$wifi", Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork},
-                {"mp_lib", "unity"}
+                {"$radio", ""},
+                {"$brand", ""},
+                {"$device", Application.platform.ToString()},
+                {"$screen_dpi", Screen.dpi},
+                {"$has_nfc", false},
+                {"$has_telephone", false},
+                {"$bluetooth_enabled", false},
+                {"$bluetooth_version", "none"}
             };
+            #if UNITY_IOS
+            properties["$manufacturer"] = "Apple";
+            properties["$ios_ifa"] = Device.advertisingIdentifier;
+            #endif
+            #if UNITY_ANDRIOD
+            properties["$google_play_services"] = "";
+            #endif
             return properties;
         }
 
-        internal static void DoEngage(Value data)
+        internal static void DoEngage(Value properties)
         {
             if (!IsTracking) return;
-            data["$token"] = MixpanelSettings.Instance.Token;
-            data["$distinct_id"] = DistinctId;
-            Enqueue(MixpanelEngageUrl, data);
+            if (AutoEngageProperties == null) AutoEngageProperties = CollectAutoEngageProperties();
+            properties.Merge(AutoEngageProperties);
+            properties["$token"] = MixpanelSettings.Instance.Token;
+            properties["$distinct_id"] = DistinctId;
+            Enqueue(MixpanelEngageUrl, properties);
+        }
+        
+        internal static Value CollectAutoEngageProperties()
+        {
+            Value properties = new Value();
+            #if UNITY_IOS
+            properties["$ios_lib_version"] = MixpanelUnityVersion;
+            properties["$ios_version"] = SystemInfo.operatingSystem;
+            properties["$ios_app_version"] = Application.version;
+            properties["$ios_app_release"] = Application.unityVersion;
+            properties["$ios_device_model"] = SystemInfo.deviceModel;
+            properties["$ios_ifa"] = Device.advertisingIdentifier;
+            #endif
+            #if UNITY_ANDRIOD
+            properties["$android_lib_version"] = MixpanelUnityVersion;
+            properties["$android_os"] = "Android";
+            properties["$android_os_version"] = SystemInfo.operatingSystem;
+            properties["$android_manufacturer"] = "";
+            properties["$android_brand"] = "";
+            properties["$android_model"] = SystemInfo.deviceModel;
+            properties["$android_app_version"] = Application.unityVersion;
+            properties["$android_app_version_code"] = Application.version;
+            #endif
+            return properties;
         }
 
         internal static double CurrentTime()
