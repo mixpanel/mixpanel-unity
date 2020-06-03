62e496a66475cfb2609f16b96e8e3919cb4d2553
Sergio Alonso Fernández <patedit@gmail.com>
2020-06-02 17:35:45 -0700

    Manual initialization capabilities (#104)

--- Mixpanel/Config.cs
+++ Mixpanel/Config.cs
@@ -6,6 +6,7 @@ namespace mixpanel
         internal static string TrackUrl = "https://api.mixpanel.com/track/?ip=1";
         internal static string EngageUrl = "https://api.mixpanel.com/engage/?ip=1";
         internal static bool ShowDebug = false;
+        internal static bool ManualInitialization = false;
         internal static float FlushInterval = 60f;
 
         internal static int BatchSize = 50;
