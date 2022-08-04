8821427f5bdfb674e206dab88a90eb2dc935e255
Jared McFarland <jared@mixpanel.com>
2022-08-04 15:09:59 -0700

    send millisecond precision timestamps (#146)
    * send millisecond precision timestamps
    
    * update MixpanelUnityVersion and CHANGELOG
    
    * use TotalMilliseconds

--- Mixpanel/Worker.cs
+++ Mixpanel/Worker.cs
@@ -17,7 +17,7 @@ namespace mixpanel
         internal static double CurrentTime()
         {
             DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            double currentEpochTime = (DateTime.UtcNow - epochStart).TotalSeconds;
+            long currentEpochTime = (long)(DateTime.UtcNow - epochStart).TotalMilliseconds;
             return currentEpochTime;
         }
 
