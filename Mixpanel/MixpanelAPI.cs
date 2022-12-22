651cff141d21a9e7651b2d3ccd092be33e152028
Zihe Jia <zihe.jia@mixpanel.com>
2022-12-21 19:59:28 -0800

    New release 3.4.0

--- Mixpanel/MixpanelAPI.cs
+++ Mixpanel/MixpanelAPI.cs
@@ -18,7 +18,7 @@ namespace mixpanel
     /// </code>
     public static partial class Mixpanel
     {
-        internal const string MixpanelUnityVersion = "3.3.2";
+        internal const string MixpanelUnityVersion = "3.4.0";
 
         /// <summary>
         /// Creates an Mixpanel instance. Use only if you have enabled "Manual Initialization" from your Project Settings.
